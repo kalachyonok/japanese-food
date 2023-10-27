717fccf415caec7988ae15494fb2be1f3a78ccf0
Olga Kalacheva <kalach2256954@gmail.com>
2023-10-27 20:12:00 +0400

    add feature: dynamic showing items amount

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon.js";
+import { useContext } from "react";
+// import { CartContextProvider } from "../../store/CartContextProvider";
+import { CartContext } from "../../store/cart-context";
 
 export const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+
+  const itemsAmount = ctx.item.reduce((sum, item) => {
+    return sum + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Корзина</span>
-      <span className={styles.badge}>2</span>
+      <span className={styles.badge}>{itemsAmount}</span>
     </button>
   );
 };
